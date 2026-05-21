6dfc19102148afbc58fbaf1ec7aafa7193d6cf6f
Fredrick Mwangi <mwangifr123@gmail.com>
2026-05-21 21:21:53 +0300

    Remove ES module import

--- server/controllers/formController.js
+++ server/controllers/formController.js
@@ -183,7 +183,7 @@ exports.meetingForm = async (req, res) => {
   }
 };
 
-import { Resend } from "resend";
+const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
